3cb20820f7b91e31825572a1f620127ac6df947a
Spectre <viktor.kovachev@antcolony.app>
2024-05-26 18:31:57 +0300

    interface for downloading and processing data

--- data.py
+++ data.py
@@ -7,29 +7,49 @@ sessions_result = None
 users_result = None
 names = None
 
-def load_data_wrapper():
+def is_session_create():
+    return sessions_result!=None
+
+def is_users_create():
+    return users_result!=None
+
+def is_events_create():
+    return events_result!=None
+
+def load_data_wrapper(load_data_done,):
     start_time = time.time()
     global data_result
     global names
     names,data_result = load_data("E://AppMetrica-data//test.csv")
     print(f"> data is loaded in {time.time() - start_time:.2f} seconds")
-    data_processing()
+    load_data_done()
 
-def data_processing():
+def data_processing(create_ui_session, create_ui_user):
     start_time = time.time()
     global events_result
     events_result = create_events(data_result, names)
     print(f"> events {len(events_result)} created in {time.time() - start_time:.2f} seconds")
 
     if "session_id" in names:
-        start_time = time.time()
-        global sessions_result
-        sessions_result = crate_sessions(events_result)
-        print(f"> sessions {len(sessions_result)} created in {time.time() - start_time:.2f} seconds")
+        create_ui_session()
 
         if "appmetrica_device_id" in names:
-            start_time = time.time()
-            global users_result
-            users_result = crate_users(sessions_result)
-            print(f"> users {len(users_result)} created in {time.time() - start_time:.2f} seconds")
+            create_ui_user()
+            
 
+def create_session(create_session_done):
+    start_time = time.time()
+    global sessions_result
+    sessions_result = crate_sessions(events_result)
+    print(f"> sessions {len(sessions_result)} created in {time.time() - start_time:.2f} seconds")
+    create_session_done()
+
+def create_users(create_user_done):
+    if not is_session_create():
+        create_session()
+    
+    start_time = time.time()
+    global users_result
+    users_result = crate_users(sessions_result)
+    print(f"> users {len(users_result)} created in {time.time() - start_time:.2f} seconds")
+    create_user_done()
--- interface.py
+++ interface.py
@@ -1,20 +1,53 @@
 import tkinter as tk
-from data import names,load_data_wrapper,data_processing
+from data import load_data_wrapper, data_processing, create_session, create_users
 
-
+def uploadin_and_processing():
+    load_data_wrapper(load_data_done)
+    data_processing(create_ui_session, create_ui_user)
 
 def create_ui():
+    global root
+    global canvas
+    global load_data_checkbox
+    global create_sessions_checkbox
+    global create_users_checkbox
+
     root = tk.Tk()
     root.title("Tkinter Example with Buttons")
 
-    canvas = tk.Canvas(root, width=300, height=200)
+    canvas = tk.Canvas(root, width=500, height=500)
     canvas.pack()
-    button1 = tk.Button(root, text="Load Data", command=load_data_wrapper)
-    canvas.create_window(50, 100, window=button1)   
-    
+
+    load_data_button = tk.Button(root, text="Load Data", command=uploadin_and_processing)
+    canvas.create_window(50, 100, window=load_data_button)
+
+    load_data_checkbox_var = tk.IntVar()
+    load_data_checkbox = tk.Checkbutton(root, text="Load Data", variable=load_data_checkbox_var, state='disabled')
+    canvas.create_window(50, 150, window=load_data_checkbox)
+
+    create_sessions_checkbox_var = tk.IntVar()
+    create_sessions_checkbox = tk.Checkbutton(root, text="Create sessions", variable=create_sessions_checkbox_var, state='disabled')
+    canvas.create_window(50, 200, window=create_sessions_checkbox)
+
+    create_users_checkbox_var = tk.IntVar()
+    create_users_checkbox = tk.Checkbutton(root, text="Create users", variable=create_users_checkbox_var, state='disabled')
+    canvas.create_window(50, 250, window=create_users_checkbox)
+ 
     root.mainloop()
-    
-def crate_data_processing_ui():
-    pass
-    
-    
+
+def load_data_done():
+    load_data_checkbox.select()
+
+def create_session_done():
+    create_sessions_checkbox.select()
+
+def create_user_done():
+    create_users_checkbox.select()
+
+def create_ui_session():
+    create_session_button = tk.Button(root, text="Create sessions", command=lambda: create_session(create_session_done))
+    canvas.create_window(150, 100, window=create_session_button)
+   
+def create_ui_user():
+    create_user_button = tk.Button(root, text="Create users", command=lambda: create_users(create_user_done))
+    canvas.create_window(250, 100, window=create_user_button)
