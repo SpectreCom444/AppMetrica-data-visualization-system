33a0acf435da96c6567c16393a820f4c6d738a75
Spectre <viktor.kovachev@antcolony.app>
2024-06-01 06:00:40 +0300

    filter for sessions and benefits

--- filters.py
+++ filters.py
@@ -1,10 +1,20 @@
 from datetime import datetime
 import constants
 
-def data_filter(data, start_date_entry, end_date_entry):
-    filtered_data = []
-    for event in data:
+class Filters:
+    def __init__(self,visualization_params):
+        self.filters_list =[]
+        self.visualization_params =visualization_params
+
+    def add_filter(self, filter):
+        self.filters_list.append(filter)
+
+    def event_verification(self, event):
+        for filter in self.filters_list:
+            if not filter(event):
+                return False
+        return True
+
+    def data_filter(self,event):
         event_datetime = event.get_value(constants.EVENT_DATATIME)
-        if start_date_entry <= event_datetime <= end_date_entry:
-            filtered_data.append(event)
-    return filtered_data
+        return self.visualization_params.start_date_entry <= event_datetime <= self.visualization_params.end_date_entry
--- visualization.py
+++ visualization.py
@@ -1,62 +1,63 @@
 
 import matplotlib.pyplot as plt
 from shared import shared_state, TypeOfData
-from filters import data_filter
+from filters import Filters
 import constants
 
 
-def counter(elements,metric_name,visualization_params):
+def counter(elements,metric_name,visualization_params, filters):
     count = {}
     if visualization_params.type_data == constants.EVENTS:
-        counter_events(elements,metric_name,visualization_params,count)   
+        counter_events(elements,metric_name,visualization_params,count,filters)   
     elif visualization_params.type_data == constants.SESSIONS:
-       counter_sessions(elements,metric_name,visualization_params,count)   
+       counter_sessions(elements,metric_name,visualization_params,count,filters)   
     elif visualization_params.type_data == constants.USERS:
-        counter_users(elements,metric_name,visualization_params,count) 
+        counter_users(elements,metric_name,visualization_params,count,filters) 
             
     return count
 
-def counter_sessions(elements,metric_name,visualization_params, count):
+def counter_sessions(elements,metric_name,visualization_params, count,filters):
     for sessions in elements:
         sessions_count={}       
-        counter_events(sessions.get_events() ,metric_name, visualization_params, sessions_count) 
+        counter_events(sessions.get_events() ,metric_name, visualization_params, sessions_count,filters) 
         for key in sessions_count.keys():
             if key in count:
                 count[key] += 1
             else:
                 count[key] = 1
 
-def counter_users(elements,metric_name,visualization_params, count):
+def counter_users(elements,metric_name,visualization_params, count,filters):
     for user in elements:
         user_count= {}
-        counter_sessions(user.get_sessions(),metric_name,visualization_params,user_count)                 
+        counter_sessions(user.get_sessions(),metric_name,visualization_params,user_count,filters)                 
         for key in user_count.keys():
             if key in count:
                 count[key] += 1
             else:
                 count[key] = 1
 
-def counter_events(elements,metric_name,visualization_params, count):
+def counter_events(elements,metric_name,visualization_params, count,filters):
     for event in elements:
-        if TypeOfData.FIELD_NAME == visualization_params.type_of_data:
-            value = event.get_value(metric_name)
-            if value in count:
-                count[value] += 1
-            else:
-                count[value] = 1
-        elif TypeOfData.TREE == visualization_params.type_of_data:
-            value = counter_events_list(event,metric_name)
-            if isinstance(value,str):
+        if filters.event_verification(event):
+            if TypeOfData.FIELD_NAME == visualization_params.type_of_data:
+                value = event.get_value(metric_name)
                 if value in count:
                     count[value] += 1
                 else:
-                    count[value] = 1             
-            else:
-                for name in value:
-                    if name in count:
-                        count[name] += 1
+                    count[value] = 1
+            elif TypeOfData.TREE == visualization_params.type_of_data:
+                value = counter_events_list(event,metric_name)
+                if isinstance(value,str):
+                    if value in count:
+                        count[value] += 1
                     else:
-                        count[name] = 1
+                        count[value] = 1             
+                else:
+                    for name in value:
+                        if name in count:
+                            count[name] += 1
+                        else:
+                            count[name] = 1
     return count
 
 def counter_events_list(event, metric_names):      
@@ -109,10 +110,12 @@ def create_chart(visualization_params):
     elif visualization_params.type_data == constants.USERS:
         data = shared_state.users_result
 
-    # if visualization_params.get_time_limits:
-    #     data= data_filter(data,visualization_params.start_date_entry,visualization_params.end_date_entry)
+    filters = Filters(visualization_params)
 
-    events_count=counter(data,visualization_params.selected_data,visualization_params)
+    if visualization_params.get_time_limits:
+        filters.add_filter(filters.data_filter)
+
+    events_count=counter(data,visualization_params.selected_data,visualization_params, filters)
 
     events_count = counting_other(events_count,visualization_params.other_reference )
 
